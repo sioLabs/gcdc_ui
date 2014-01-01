9f8f5582374c397b8a7971017b308c60fe47bc00
Ashutosh Singh <dh_ashu@outlook.com>
2014-01-01 10:23:41 +0530

    Sync on both PC's

---
+++ war/js/gcal.js
@@ -0,0 +1,287 @@
+
+var clientId = '296331844386-34pj6h3emkir85tt09ptip1ponfuqnso.apps.googleusercontent.com';
+var apiKey = 'AIzaSyB58_n-hiIRJ1FizLyqU810ihWjY0Sku1Q';
+var scopes = 'https://www.googleapis.com/auth/calendar';
+var data = [];
+
+function handleClientLoad() {
+	  gapi.client.setApiKey(apiKey);
+	  window.setTimeout(checkAuth,1);
+	  checkAuth();
+	}
+
+	function checkAuth() {
+	  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+	      handleAuthResult);
+	}
+
+	function handleAuthResult(authResult) {
+	  var authorizeButton = document.getElementById('authorize-button');
+	  if (authResult) {
+	    authorizeButton.style.visibility = 'hidden';
+	    makeApiCall();
+	  } else {
+	    authorizeButton.style.visibility = '';
+	    authorizeButton.onclick = handleAuthClick;
+	   }
+	}
+
+	function handleAuthClick(event) {
+	  gapi.auth.authorize(
+	      {client_id: clientId, scope: scopes, immediate: false},
+	      handleAuthResult);
+	  return false;
+	}
+	
+
+	
+	function makeApiCall() {
+		  
+	    gapi.client.load('calendar', 'v3', function() {
+		    //insert into my calendar
+//		    var resource = {
+//		    		  "summary": "Vaccinate your child with",
+//		    		  "location": "England",
+//		    		  "start": {
+//		    		    "date":"2013-12-14"
+//		    		  },
+//		    		  "end": {
+//		    		    "date":"2013-12-14"
+//		    		    }
+//		    		  };
+	    	
+	    	for (var i = 0;i<data.length;i++){
+	    		var resource = getEventObject(data[i]);
+	    		var request = gapi.client.calendar.events.insert({
+			    	  'calendarId': 'primary',
+			    	  'resource': resource
+			    	});
+			    
+			    request.execute(function(resp){
+			    	console.log(resp);
+			    });
+	    		
+	    	}
+	    	
+		    
+		    
+		    
+		    
+		
+//			     request = gapi.client.calendar.events.list({
+//			      'calendarId': 'primary'
+//			    });
+		    
+		          
+//		    request.execute(function(resp) {
+//		      for (var i = 0; i < resp.items.length; i++) {
+//		        var li = document.createElement('li');
+//		        li.appendChild(document.createTextNode(resp.items[i].summary));
+//		        document.getElementById('events').appendChild(li);
+//		      }
+//		    });
+		  });
+		}
+	
+	function getEventObject(data1){
+		
+		console.log(data1);
+		
+		var summary = "Vaccinate your child with : " 
+			for(var i = 0 ;i <data1.vaccination.length; i++){
+				summary += data1.vaccination[i] + ", ";
+			}
+		var resource = {
+				"summary" : summary,
+				"start" : {
+					"date" : data1.date
+				},
+				"end" :{
+					"date" : data1.date
+				}
+		
+		};
+		
+		return resource;
+		
+	}
+	
+	
+	function getVaccinationSchedule(dob){
+		
+		//alert('date is'+dob);
+
+		var data = [
+		            {
+		              "date":Date.parse(dob).addDays(1).toString("yyyy-MM-dd"),
+		              "vaccination" : ["BCG - Injection", "Hep B-1"]    		            	
+		            },
+		        {
+		           	 "date" :Date.parse(dob).addDays(41).toString("yyyy-MM-dd"),
+		           	 "vaccination" : ["DTP 1", "IPV -1","Hep-B 2","Hib 1","Rotavirus 1", "PCV 1" ] 
+		            },
+		         {
+   		           	 "date" :Date.parse(dob).addDays(69).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["DTP -2 ", "IPV -2", "Hib 2", "Rotavirus 2", "PCV 2"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(97).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["DTP -3", "IPV -3", "Hib 3", "Rotavirus 3", "PCV 3"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(181).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["OPV 1", "Hep-B 3"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(272).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["OPV 2", "Measles"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(364).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["Hep-A 1"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(455).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["MMR 1", "Varicella 1", "PCV booster"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(486).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["DTP B!", "IPT B1", "Hib B1"] 
+   		            },
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(547).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["Hep-A 2"] 
+   		            },
+   		            
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(729).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["Typhoid 1"] 
+   		            },
+   		            
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(1642).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["DTP B2", "OPV 3", "MMR 2", "Varicella 2", "Typhoid 2"] 
+   		            },
+   		            
+   		         {
+   		           	 "date" :Date.parse(dob).addDays(3651).toString("yyyy-MM-dd"),
+   		           	 "vaccination" : ["TDAP", "HPV"] 
+   		            },
+		            
+		            ];
+				
+		
+		return data;
+	}
+	
+	
+function getPersonalVaccinationSchedule(dob){
+		
+		//alert('date is'+dob);
+
+		var data = [
+		            {
+		              "age" : "<strong>Birth</strong>",
+		              "date":Date.parse(dob).addDays(1).toString("dd-MM-yyyy"),
+		              "vaccination" : ["BCG - Injection", "Hep B-1"],
+		              "comments" : ""
+		            },
+		        {
+		            	"age" : "<strong>6 Weeks</strong>",
+		           	 "date" :Date.parse(dob).addDays(41).toString("dd-MM-yyyy"),
+		           	 "vaccination" : ["DTP 1", "IPV -1","Hep-B 2","Hib 1","Rotavirus 1", "PCV 1" ],
+		             "comments" : " <strong>Polio :</strong> Use IPV. But may be replaced with OPV if former is unaffordable/unavailable<br /> <strong>Rotavirus :</strong> 2 doses of RV-1 and 3 doses of RV-5"
+		            },
+		         {
+		            	"age" : "<strong>10 Weeks</strong>",
+   		           	 "date" :Date.parse(dob).addDays(69).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["DTP -2 ", "IPV -2", "Hib 2", "Rotavirus 2", "PCV 2"],
+   		          "comments" : ""
+		            
+   		            },
+   		         {
+   		            	"age" : "<strong>14 Weeks</strong>",
+   		           	 "date" :Date.parse(dob).addDays(97).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["DTP -3", "IPV -3", "Hib 3", "Rotavirus 3", "PCV 3"],
+   		          "comments" : "<strong>Rotavirus :</strong> Only 2 doses of RV1 are recommended at present."
+   		            },
+   		         {
+   		            	"age" : "<strong>6 Months</strong>",
+   		           	 "date" :Date.parse(dob).addDays(181).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["OPV 1", "Hep-B 3"],
+   		          "comments" : "<strong>Hepatitis-B :</strong> The final (third or fourth) dose in the HepB vaccine series should be administered no earlier than age 24 weeks and at least 16 weeks after the first dose."
+   		            },
+   		         {
+   		            	"age" : "<strong>9 Months</strong>",
+   		           	 "date" :Date.parse(dob).addDays(272).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["OPV 2", "Measles"],
+   		          "comments" : ""
+   		            },
+   		         {
+   		            	"age" : "<strong>12 Months</strong>",
+   		           	 "date" :Date.parse(dob).addDays(364).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["Hep-A 1"],
+   		          "comments" : "<strong>Hepatitis A :</strong> For both killed and live hepatitis-A vaccines, 2 doses are recommended"
+   		            },
+   		         {
+   		            	"age" : "<strong>15 Months</strong>",
+   		           	 "date" :Date.parse(dob).addDays(455).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["MMR 1", "Varicella 1", "PCV booster"],
+   		          "comments" : "<strong>Varicella :</strong> The risk of breakthrough varicella is lower if given 15 months onwards."
+   		            },
+   		         {
+   		            	"age" : "<strong>16-18 Months</strong>",
+   		           	 "date" :Date.parse(dob).addDays(486).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["DTP B!", "IPT B1", "Hib B1"],
+   		          "comments" : "The first booster (4th dose) may be administered as early as age 12 months, provided at least 6 months have elapsed since the third dose."
+   		            },
+   		         {
+   		            	"age" : "<strong>18 Months</strong>",
+   		           	 "date" :Date.parse(dob).addDays(547).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["Hep-A 2"],
+   		          "comments" : "<strong>Hepatitis A :</strong> For both killed and live hepatitis-A vaccines 2 doses are recommended"
+   		            },
+   		            
+   		         {
+   		            	"age" : "<strong>2 Years</strong>",
+   		           	 "date" :Date.parse(dob).addDays(729).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["Typhoid 1"],
+   		          "comments" : "<strong>Typhoid :</strong> Typhoid revaccination every 3 years, if Vi-polysaccharide vaccine is used."
+   		            },
+   		            
+   		         {
+   		            	"age" : "<strong>4.5 - 5 Years</strong>",
+   		           	 "date" :Date.parse(dob).addDays(1642).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["DTP B2", "OPV 3", "MMR 2", "Varicella 2", "Typhoid 2"],
+   		          "comments" : "<strong>MMR :</strong> the 2nd dose can be given at anytime 4-8 weeks after the 1st dose. <br /><strong>Varicella :</strong> the 2nd dose can be given at anytime 3 months after the 1st dose."
+   		            },
+   		            
+   		         {
+   		            	"age" : "<strong>10-12 Years</strong>",
+   		           	 "date" :Date.parse(dob).addDays(3651).toString("dd-MM-yyyy"),
+   		           	 "vaccination" : ["TDAP", "HPV"],
+   		          "comments" : "<strong>Tdap :</strong> is preferred to Td followed by Td every 10 years.<br/> <strong>HPV :</strong> Only for females, 3 doses at 0, 1-2 (depending on brands) and 6 months."
+   		            },
+		            
+		            ];
+				
+		
+		return data;
+	}
+	
+	
+	
+	$(document).ready(function () {
+		  //	
+	$('#schedulerBtn').click(function(){
+	
+		var dob = $('#dob').val();
+		  data = getVaccinationSchedule(dob);
+	handleClientLoad();
+	handleAuthClick();
+		
+		return;
+	});
+	
+	//
+	});
---
+++ war/scheduler.jsp
@@ -0,0 +1,189 @@
+<!doctype html>
+<html  lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">    
+    <link rel="stylesheet" href="css/foundation-datepicker.css" date-date-format="dd-mm-yyyy" />
+    <script src="js/modernizr.js"></script>
+  </head>
+  <body>
+  <div class="fixed">
+  <nav class="top-bar" data-topbar>
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="/index.html">Vaccinate</a></h1>
+    </li>
+    
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="active"><a href="#">Hello</a></li>
+      <li><a href="#aboutus">Who are we?</a></li>
+      
+      <li class="has-dropdown">
+        <a href="#">Vaccinate</a>
+        <ul class="dropdown">
+          <li><a href="scheduler.jsp">Scheduler</a></li>
+          <li><a href="locator.html">Locator</a></li>
+          <li><a href="consult.html">Consultation</a></li>
+          
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Left Nav Section -->
+    
+  </section>
+</nav>
+</div>
+
+<!-- content of scheduler -->
+
+<div class="row">
+	<div class="large-12 columns">
+		<h3>Get the <strong>Personalized</strong> schedule of vaccination for your child</h3>
+		<h4><strong>Save</strong> it. <strong>Download</strong> it. Add it your calendar in one Click</h4>
+	</div>
+	
+	<hr />
+	<div class="large-12 columns">
+		<div class="large-2 columns">
+			<input type="text" placeholder="Child's Name" id="childName"/>
+		</div>
+		<div class="large-2 columns ">
+		<input type="text" placeholder="Date of Birth" id="datepicker"/>
+		</div>
+		<div class="large-2 columns">
+		<a href="#" class="button radius tiny" id="generate">View Schedule</a>
+		</div>	
+		
+		<div class="large-6 columns push-1">
+			<a href="#" class="button tiny" id="pdf">Download</a>
+			<a href="#" class="button tiny">Add to Google Drive</a>
+			<a href="#" class="button tiny">Add to Calendar</a>
+		</div>
+		
+	</div>
+	
+	<hr/>
+	
+	<div class="large-12 columns" id="chart">
+	
+	
+	</div>
+	
+	
+	
+	
+	
+	
+</div>
+
+<!-- content ends -->
+
+
+
+
+
+  <!-- Footer -->
+  
+  <footer class="row">
+    <div class="large-12 columns">
+      <hr />
+      <div class="row">
+        <div class="large-6 columns">
+          <p>© Copyright no one at all. Go to town.</p>
+        </div>
+        <div class="large-6 columns">
+          <ul class="inline-list right">
+            <li><a href="index.html#aboutus">About Vaccinate</a></li>
+            <li><a href="index.html#feedback">Feedback</a></li>
+            <li><a href="faqs.html">FAQs</a></li>
+            <li><a href="volunteer.html">Volunteer</a></li>
+          </ul>
+        </div>
+      </div>
+    </div> 
+  </footer>
+
+
+
+
+   
+    
+        
+    <script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/foundation.topbar.js"></script>
+    <script src="js/foundation-datepicker.js"></script>
+    <script src="js/gcal.js"></script>
+    <script src="js/date.js"></script>
+    <script src="https://raw.github.com/MrRio/jsPDF/master/dist/jspdf.min.js"></script>
+    <!-- <script src="js/foundation.orbit.js"></script> -->
+    
+    <script>
+    	$(document).foundation();
+      function showTable(schedule){
+    	  var startContent = "<div class='large-3 columns'><strong> Child Name: </div><div class='large-3 columns pull-1' style='text-transform:uppercase'>"+ $('#childName').val() + "</strong> </div> <div class='large-3 columns'> <strong>Date of Birth : </div><div class='large-3 columns pull-1'>" + $('#datepicker').val() + "</strong></div> <br/> <hr />"; 
+    	  
+    	  var tableContent =  startContent+ "<table> <thead> <tr> <th> Age </th> <th width =\"100\"> Due Date </th> <th width=\"170\"> Vaccine </th> <th> Comments </th> </tr> </thead> <tbody> ";
+    	  
+    	  for(var i = 0 ; i <schedule.length ; i++){
+    		  tableContent += "<tr> <td>"
+    		  tableContent += schedule[i].age + "</td><td>";
+    		  tableContent += schedule[i].date + "</td><td>";
+    		  
+    		  for(var j = 1 ; j<= schedule[i].vaccination.length ; j++){
+    			  tableContent += "<strong>"+ j +". "+ schedule[i].vaccination[j-1]+"</strong> <br/>"
+    		  }    		  
+    		  
+    		  tableContent += "</td><td>";
+    		  tableContent += schedule[i].comments;    		  
+    		  tableContent += "</td></tr>"
+    		  
+    	  }    	  
+    	  tableContent += "</tbody> </table>";
+    	  
+    	  $('#chart').html(tableContent);
+    	  
+      }
+    
+      
+      $('#datepicker').click(function(){
+    	 $('#datepicker').fdatepicker({
+    		 format:'mm-dd-yyyy'
+    	 }); 
+      });
+      //code for the generate function click
+      $('#generate').click(function(){
+    	  var dob = $('#datepicker').val();
+    	  var schedule = getPersonalVaccinationSchedule(dob);
+    	  showTable(schedule);
+    	  
+      });
+      
+      $('#pdf').click(function(){
+    	  
+    	  var doc=new jsPDF();
+    	  var specialElementHandlers = {
+    		        '#editor': function (element, renderer) {
+    		            return true;
+    		        }
+    		    };
+    	  doc.fromHTML($('#chart').get(0), 15, 15, {
+              'width': 170,
+                  'elementHandlers': specialElementHandlers
+          });
+    	  var name = $('#childname').val() + '.pdf';
+          doc.save(name);
+    	  
+      });
+    	 
+    </script>
+  </body>
+</html>
