e268b78cc2584b7e5a1ed9a38469a3c95120ac33
Sio Kumar <dh_ashu@hotmail.com>
2014-01-05 02:42:34 +0530

    Starting working on the pdf generator for the scheduler.

--- src/com/osahub/gcdc/pdf/SchedulerPdfCreator.java
+++ src/com/osahub/gcdc/pdf/SchedulerPdfCreator.java
@@ -44,21 +44,47 @@ public class SchedulerPdfCreator extends HttpServlet {
 		
 		  document.open();
 
-		
-          PdfPTable table = new PdfPTable(3); // 3 columns.
-
-          PdfPCell cell1 = new PdfPCell(new Paragraph("Cell 1"));
-          PdfPCell cell2 = new PdfPCell(new Paragraph("Cell 2"));
-          PdfPCell cell3 = new PdfPCell(new Paragraph("Cell 3"));
-
-          table.addCell(cell1);
-          table.addCell(cell2);
-          table.addCell(cell3);
-
           try {
-        	  document.add(new Paragraph("Heelo World Here"));
+		
+          PdfPTable table = new PdfPTable(4); // 3 columns.
+          
+          table.setSpacingBefore(10f);
+          float colWidths[] = {20f,20f,30f,40f};
+          table.setWidths(colWidths);
+          
+          PdfPCell head1 =  new PdfPCell(new Paragraph("Age"));
+          PdfPCell head2 =  new PdfPCell(new Paragraph("Due Date"));
+          PdfPCell head3 =  new PdfPCell(new Paragraph("Vaccinations"));
+          PdfPCell head4 =  new PdfPCell(new Paragraph("Remarks/Comments"));
+          
+          String[] age = new  String[]{
+        	  "Birth", "6 Weeks", "10 Weeks", "14 Weeks", "6 months", "9 Months", "12 Months", "15 Months", "16-18 Months",
+        	  "18 Months", "2 Years", "4.5-5 Years", "10-12 Years"
+          };
+          
+          String[][] vaccines = new String[][]{
+        		  {"B.C.G", "Hep-B 1"}, {"DTP 1", "IPV 1", "Hep-B 2", "Hib 1", "Rotavirus 1", "PCV 1"},
+        		  {"DTP 2", "IPV 2", "Hib 2", "Rotavirus 2", "PCV 2"},{"DTP 3", "IPV 3", "Hib 3", "Rotavirus 3", "PCV 3"},
+        		  {"OPV 1", "Hep-B 3"},{"OPV 2", "Measles"}, {"Hep-A 1"}, {"MMR 1", "Varicella 1", "PCV Booster"},
+        		  {"DTP B1", "IPV B1", "Hib B1"}, {"Hep-A 2"}, {"Typhoid 1"}, {"DTP B2", "OPV 3", "MMR2", "Varicella2", "Typhoid2"},
+        		  {"Tdap/Td", "HPV"}
+        		  
+          };
+          
+          String[] comments = new String[]{
+        		 
+          };
+          
+          
+          
+          table.addCell(head1);
+          table.addCell(head2);
+          table.addCell(head3);
+          table.addCell(head4) ;         
+          
+          document.add(new Paragraph("Heelo World Here"));
 			document.add(table);
-		} catch (DocumentException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("Error in creating table");
 			e.printStackTrace();
@@ -74,11 +100,7 @@ public class SchedulerPdfCreator extends HttpServlet {
           os.flush();
           os.close();
           
-           
-          
-
-         
-          
+ 
 	}
 	
 
